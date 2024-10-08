4740c4c9eaaaae5bc14ab67252e57ac66b5c7afd
k2nq2 <166155579+k2nq2@users.noreply.github.com>
2024-10-08 14:45:45 +0300

    Add files via upload

---
+++ js/accordeon.js
@@ -0,0 +1,40 @@
+const accordItem = document.querySelectorAll(".accordion-item");
+const programList = document.querySelector(".program-list");
+const programListItem = document.querySelector(".program-list");
+const programBtn = document.querySelector(".open-program-btn");
+const programBtnText = document.querySelector(".program-btn-text");
+const programBtnImg = document.querySelector(".program-btn-img");
+accordItem.forEach((e) => {
+    e.addEventListener("click", toggleItem, false);
+});
+
+function toggleItem() {
+    const isActive = this.classList.contains("active");
+
+    accordItem.forEach((e) => {
+        if (e.classList.contains("active")) {
+            e.classList.remove("active")
+        }
+    })
+
+    if (!isActive) {
+        this.classList.add("active")
+    }
+}
+
+ programBtn.addEventListener("click", toggleList, false);
+
+ function toggleList() {
+     const isExpanded = programList.classList.contains("collapse");
+
+     if (isExpanded) {
+         programList.classList.remove("collapse")
+         this.classList.add("expanded")
+         programBtnImg.classList.remove("collapsebtn")
+         programBtnText.textContent = "Дивитися всi вiдгуки"; 
+     } else {
+         programList.classList.add("collapse")
+         this.classList.remove("expanded")
+         programBtnImg.classList.add("collapsebtn")
+         programBtnText.textContent = "Закрити відгуки";
+     } }
